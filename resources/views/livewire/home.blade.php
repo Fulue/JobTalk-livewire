5525f07b2d076f19e95da2b21fdaad5a99cf9182
Fulue <45338196+Fulue@users.noreply.github.com>
2024-09-21 18:15:59 +0400

    feat: Add new section with icon blocks on home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -4,14 +4,14 @@
             <!-- Title -->
             <div class="mt-5 max-w-xl text-center mx-auto">
                 <h1 class="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
-                    Подготовка к собеседованию
+                    Подготовка к собеседованию в IT
                 </h1>
             </div>
             <!-- End Title -->
 
             <div class="mt-5 max-w-3xl text-center mx-auto">
                 <p class="text-lg text-gray-600 dark:text-neutral-400">
-                    Сервис с вопросами и ответами на самые распространенные вопросы собеседований
+                    Сервис с вопросами и ответами для подготовки к техническим интервью в IT.
                 </p>
             </div>
 
@@ -59,5 +59,75 @@
         <!-- End Grid -->
     </div>
     <!-- End Card Section -->
+
+    <!-- Icon Blocks -->
+    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+
+        <!-- Title -->
+        <div class="max-w-2xl text-center mx-auto mb-10 lg:mb-14">
+            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Преимущества сервиса</h2>
+        </div>
+        <!-- End Title -->
+
+        <div class="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
+            <!-- Icon Block -->
+            <div>
+                <div class="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-neutral-900">
+                    <svg class="shrink-0 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z"/><path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1"/></svg>
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-semibold text-gray-800 dark:text-white">Актуальные вопросы </h3>
+                    <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                        Мы обновляем базу вопросов, следя за новыми требованиями рынка.
+                    </p>
+                </div>
+            </div>
+            <!-- End Icon Block -->
+
+            <!-- Icon Block -->
+            <div>
+                <div class="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-neutral-900">
+                    <svg class="shrink-0 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-semibold text-gray-800 dark:text-white">Глубокие объяснения</h3>
+                    <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                        Каждый вопрос сопровождается детализированными объяснениями.
+                    </p>
+                </div>
+            </div>
+            <!-- End Icon Block -->
+
+            <!-- Icon Block -->
+            <div>
+                <div class="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-neutral-900">
+                    <svg class="shrink-0 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 7h-9"/><path d="M14 17H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-semibold text-gray-800 dark:text-white">Реальные задачи</h3>
+                    <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                        Вопросы основаны на реальных интервью из IT-компаний.
+                    </p>
+                </div>
+            </div>
+            <!-- End Icon Block -->
+
+            <!-- Icon Block -->
+            <div>
+                <div class="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-neutral-900">
+                    <svg class="shrink-0 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="10" height="14" x="3" y="8" rx="2"/><path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4"/><path d="M8 18h.01"/></svg>
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-semibold text-gray-800 dark:text-white">Удобный интерфейс</h3>
+                    <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                        Простая навигация и фильтрация по профессиям.
+                    </p>
+                </div>
+            </div>
+            <!-- End Icon Block -->
+        </div>
+    </div>
+    <!-- End Icon Blocks -->
+
 
 </div>

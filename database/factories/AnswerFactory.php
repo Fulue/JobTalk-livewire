cdfd9308ff76a89fb124ca307520c977b1e8d293
Fulue <45338196+Fulue@users.noreply.github.com>
2024-09-24 08:06:37 +0400

    refactor: Update AnswerFactory source field generation probability

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -71,7 +71,7 @@ class AnswerFactory extends Factory
         // Возвращаем финальный ответ
         return [
             'answer' => trim($answerText), // Генерация ответа из 5-15 предложений
-            'source' => $this->faker->url,
+            'source' => $this->faker->optional(0.7)->url,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];

65230975860b6386bac1b0de3369b21b49bacc0e
Fulue <45338196+Fulue@users.noreply.github.com>
2024-10-20 18:00:30 +0400

    refactor: Create taggables and question_timestamp tables, add similarity column

---
+++ database/migrations/0001_01_01_000011_create_question_timestamp_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_timestamp', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignUuid('timestamp_id')->nullable()->constrained()->onDelete('cascade'); // Внешний ключ на timestamps
+            $table->foreignUuid('question_id')->nullable()->constrained()->onDelete('cascade'); // Внешний ключ на questions
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_timestamp');
+    }
+};

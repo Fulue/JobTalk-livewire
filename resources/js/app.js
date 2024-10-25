8ae7863e5b4141adffa20a1fd5da7bc85708c005
Fulue <45338196+Fulue@users.noreply.github.com>
2024-10-25 11:57:42 +0400

    feat: add smooth scrolling and custom scrollbar styles
    Adds a new Livewire event `scrollToTop` that scrolls the page to the
    top with a smooth animation. Also, adds custom styles for the
    scrollbar, including a dark theme variant.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,11 @@ import Clipboard from '@ryangjchandler/alpine-clipboard'
 Alpine.plugin(Clipboard)
 
 Livewire.start()
+
+
+Livewire.on('scrollToTop', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
